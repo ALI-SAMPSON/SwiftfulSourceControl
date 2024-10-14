1480f1e61239313a184feaa4dfc98b2b9737a138
Sampson <alisampson2@gmail.com>
2024-10-14 14:06:08 +0000

    [Feature]

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = "dfhgjhjhhlkj"
+    @State private var title: String = "Hello, world!"
     var body: some View {
         VStack {
             Text("Hi")
+            Text("Screen 1!")
             Text("Screen 2!")
             Text("Screen 3!")
         }
