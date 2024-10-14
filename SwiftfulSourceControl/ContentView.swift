40016993e80a66ff6bdbcdcce76bca247d6878ee
Sampson <alisampson2@gmail.com>
2024-10-14 12:35:25 +0000

    [Clean] Add commit messsage documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by Sampson Awenbolimi Ali on 12/10/2024.
 //
 
+/*
+ 
+ WRITING GOOD COMMIT MESSAGES
+ 
+ NEW FEEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION
+ [Patch] Descriptioin of patch
+ 
+ BUG NOT IN PRODUCTION
+ [Bug] Descriptioin of bug
+ 
+ MUNDANE TASKS
+ [Clean] Description of changes
+ 
+ RELEASE
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
@@ -13,13 +34,13 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Swiftful Thinking!")
+            Text("Swiftful!")
         }
         .padding()
         
         Button("Click me") {
             
-        }.background(Color.red)
+        }.background(Color.green)
     }
 }
 
