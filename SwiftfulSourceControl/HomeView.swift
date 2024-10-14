c5a7165c76814eceb1e1152bdacbc33ed2381426
Sampson <alisampson2@gmail.com>
2024-10-14 12:21:56 +0000

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Sampson Awenbolimi Ali on 14/10/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
