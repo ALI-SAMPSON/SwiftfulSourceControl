d723f98c0d5b30450994eb25dc0b03910414a10a
Sampson <alisampson2@gmail.com>
2024-10-28 11:43:08 +0000

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Sampson Awenbolimi Ali on 28/10/2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
